41206c1dd917d28d301d81b5a4217a4e2361e575
RebecaCamilo <rebecacamilo3@gmail.com>
2021-11-19 17:33:54 -0300

    Cors configurado - com problemas

--- formacao/settings.py
+++ formacao/settings.py
@@ -37,14 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
     'rest_framework',
     'accounts',
 ]
+
+CORS_ALLOWED_ORIGINS = [http://127.0.0.1:3000] ##### LINK NÃO ESTÁ CERTO
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
